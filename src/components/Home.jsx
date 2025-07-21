200c0a0e56d55b9a4390fef6a351d6c6d0feb084
YuriSP <toastysp@YuriSPs-MacBook-Pro.local>
2025-07-21 14:45:53 -0300

    finally woke up

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/Home.css'
+
+export default function Home() {
+  return (
+    <section className="home">
+      <h1>WAKE UP</h1>
+      <p>MAIOR・CONFERÊNCIA・DE・JOVENS・DA・ZONA・SUL</p>
+      <h2>WAKE UP 2K25</h2>
+    </section>
+  );
+}
