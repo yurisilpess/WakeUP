931c7e98fd930a588b677f9265d39335b653c2c3
YuriSP <toastysp@YuriSPs-MacBook-Pro.local>
2025-07-22 18:39:55 -0300

    base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import '../styles/Home.css'
 
 export default function Home() {
   return (
-    <section className="home">
-      <div className="WU_sticky">WAKE UP 2K25</div>
-      <h1>WAKE UP</h1>
-      <p>MAIOR・CONFERÊNCIA・DE・JOVENS・DA・ZONA・SUL</p>
-
-      <p>MAIS DE 1000 JOVENS</p>
-    </section>
+    <section className="hero">
+  <div className="hero-media">
+    {/* Add a logo image or video later */}
+    <img src="/images/logo.png" alt="WAKE UP Logo" />
+  </div>
+  <p className="hero-subtitle">MAIOR・CONFERÊNCIA・DE・JOVENS・DA・ZONA・SUL</p>
+  <h1 className="hero-title">WAKE UP 2K25</h1>
+</section>
+  
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ import App from './components/Home'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
