a7175b95fae27cafccfbb98dba1fa6ea59fbb739
YuriSP <toastysp@YuriSPs-MacBook-Pro.local>
2025-07-22 18:12:18 -0300

    Fonts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,11 @@ import '../styles/Home.css'
 export default function Home() {
   return (
     <section className="home">
+      <div className="WU_sticky">WAKE UP 2K25</div>
       <h1>WAKE UP</h1>
       <p>MAIOR・CONFERÊNCIA・DE・JOVENS・DA・ZONA・SUL</p>
-      <h2>WAKE UP 2K25</h2>
+
+      <p>MAIS DE 1000 JOVENS</p>
     </section>
-  );
+  )
 }
